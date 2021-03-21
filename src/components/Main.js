0939f9ef8f826c5528d121dc7992f6723d2247ef
James Yunana <gajejames@gmail.com>
2021-03-21 16:46:26 +0100

    Testing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 import hero from "../svg/hero.svg";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Main = () => {
     return(
