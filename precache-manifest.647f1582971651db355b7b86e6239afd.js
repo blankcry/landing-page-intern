3d767db644b5768d644d6e26a1853bbedc273e89
James Yunana <gajejames@gmail.com>
2021-03-21 16:23:11 +0100

    Updates

---
+++ precache-manifest.647f1582971651db355b7b86e6239afd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73bc33301b03df50e2a2b8816b8fb584",
+    "url": "/blankcry/landing-page-intern.git/index.html"
+  },
+  {
+    "revision": "60623d965bb02978d681",
+    "url": "/blankcry/landing-page-intern.git/static/css/main.7c0a551b.chunk.css"
+  },
+  {
+    "revision": "08c3dc9de9f5350884cf",
+    "url": "/blankcry/landing-page-intern.git/static/js/2.086eb93e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/blankcry/landing-page-intern.git/static/js/2.086eb93e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60623d965bb02978d681",
+    "url": "/blankcry/landing-page-intern.git/static/js/main.7928f47c.chunk.js"
+  },
+  {
+    "revision": "2d3c44c234fcb7d7b058",
+    "url": "/blankcry/landing-page-intern.git/static/js/runtime-main.1afcdfa3.js"
+  },
+  {
+    "revision": "bed44b0a3d2e655853f01a06fee697bb",
+    "url": "/blankcry/landing-page-intern.git/static/media/GrowWtihUs_1072x603.jpg.web.536.402.bed44b0a.jpeg"
+  },
+  {
+    "revision": "0e48089ff38fb96681fd068935617a77",
+    "url": "/blankcry/landing-page-intern.git/static/media/hero.0e48089f.svg"
+  },
+  {
+    "revision": "e992addfda20b8beddafbb50c7674cf9",
+    "url": "/blankcry/landing-page-intern.git/static/media/istockphoto-1145371340-612x612.e992addf.jpg"
+  }
+]);
